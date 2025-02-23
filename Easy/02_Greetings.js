e51d5b27ff282685f48c9c358d31bc303fa1c7d9
Md Kamrul Hasan <kamrul.phy@gmail.com>
2025-02-23 07:29:09 +0600

    problem 2 solved

---
+++ Easy/02_Greetings.js
@@ -0,0 +1,31 @@
+//Greetings
+//Problem Statement
+/*
+Write a program that asks the user for their name and then prints a personalized greeting.
+
+Input
+The input consists of a string 
+S
+S
+
+Output
+Output a single line containing the personalized greeting.
+
+Constraints
+1 ≤ S ≤ 1000
+Example:
+What is your name?
+
+Input:
+Alice
+Output:
+Hello, Alice! Nice to meet you.
+
+*/
+
+//Solution
+
+function main(input) {
+  console.log("Hello, " + input + "! Nice to meet you.");
+}
+main("alice");
